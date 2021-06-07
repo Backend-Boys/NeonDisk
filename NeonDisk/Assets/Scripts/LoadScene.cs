c7243076c78a030ab3ba06ea3bf4204ec3e681de
Jacob <jacobsreturn@hotmail.com>
2021-06-07 12:29:29 +1000

    Adding shaders and menu items.

---
+++ NeonDisk/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void Load(string a_sceneName)
+    {
+        SceneManager.LoadSceneAsync(a_sceneName);
+    }
+}
